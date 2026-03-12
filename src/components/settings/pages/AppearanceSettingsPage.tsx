2a59f090c951b054843e54d85a0bc16912983b88
AdithyaAlokkan <adithya.alokkan@gmail.com>
2026-03-12 01:57:46 +0000

    fix(settings): change theme mode input to select from toggle group

--- src/components/settings/pages/AppearanceSettingsPage.tsx
+++ src/components/settings/pages/AppearanceSettingsPage.tsx
@@ -6,10 +6,23 @@ import {
   FieldSet,
 } from '@/components/ui/field'
 import { useSettings } from '@/shared/Settings/SettingsContext'
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 const AppearanceSettingsPage = () => {
   const { settings, setSettings } = useSettings()
+
+  const itemsThemeMode = [
+    { label: 'Dark', value: 'dark' },
+    { label: 'System', value: 'system' },
+    { label: 'Light', value: 'light' },
+  ]
 
   return (
     <FieldGroup className='p-12'>
@@ -18,30 +31,30 @@ const AppearanceSettingsPage = () => {
         <FieldGroup>
           <Field orientation={'horizontal'}>
             <FieldLabel htmlFor='input_darkMode'>Mode</FieldLabel>
-            <ToggleGroup
-              variant={'outline'}
-              value={[settings.theme.mode]}
+            <Select
+              items={itemsThemeMode}
+              value={settings.theme.mode}
               onValueChange={(value) => {
                 if (!value) return
                 setSettings({
                   ...settings,
-                  theme: {
-                    ...settings.theme,
-                    mode: value[0] as 'dark' | 'light' | 'system',
-                  },
+                  theme: { ...settings.theme, mode: value },
                 })
               }}
             >
-              <ToggleGroupItem value='dark' aria-label='toggle dark'>
-                Dark
-              </ToggleGroupItem>
-              <ToggleGroupItem value='system' aria-label='toggle system'>
-                System
-              </ToggleGroupItem>
-              <ToggleGroupItem value='light' aria-label='toggle light'>
-                Light
-              </ToggleGroupItem>
-            </ToggleGroup>
+              <SelectTrigger className='w-24'>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent className={'w-25 min-w-25'}>
+                <SelectGroup>
+                  {itemsThemeMode.map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.label}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </Field>
         </FieldGroup>
       </FieldSet>
