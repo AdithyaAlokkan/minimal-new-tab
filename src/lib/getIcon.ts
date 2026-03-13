60aeec8060a4649e1688f9f188f14f1c8c332ff3
AdithyaAlokkan <adithya.alokkan@gmail.com>
2026-03-13 18:02:11 +0000

    chore: format document with prettier and normalize line endings accross the project

--- src/lib/getIcon.ts
+++ src/lib/getIcon.ts
@@ -6,7 +6,6 @@ const handleError = (error: unknown) => {
   console.log('Error on sending message: ', error)
 }
 
-
 export default function getIcon(url: string) {
   const message = chrome.runtime.sendMessage({
     type: 'fetchIcons',
