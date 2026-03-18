4809b73a024a8d0d5d2e687664ea7a80ac7f2371
AdithyaAlokkan <adithya.alokkan@gmail.com>
2026-03-18 13:45:26 +0000

    fix(settings): hide color options in theme for modes other than custom

--- src/components/settings/pages/AppearanceSettingsPage.tsx
+++ src/components/settings/pages/AppearanceSettingsPage.tsx
@@ -64,168 +64,172 @@ const AppearanceSettingsPage = () => {
           </Field>
         </FieldGroup>
       </FieldSet>
-      <FieldSeparator />
-      <FieldSet>
-        <FieldLegend>Color scheme</FieldLegend>
-        <FieldGroup>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Background</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.background}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      background: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Foreground</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.foreground}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      foreground: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Card</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.card}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      card: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Primary</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.primary}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      primary: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Secondary</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.secondary}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      secondary: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Accent</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.accent}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      accent: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation={'horizontal'}>
-            <FieldLabel>Destructive</FieldLabel>
-            <Input
-              type='color'
-              value={settings.theme.color.destructive}
-              onValueChange={(value) => {
-                if (!value) return
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: {
-                      ...settings.theme.color,
-                      destructive: value,
-                    },
-                  },
-                })
-              }}
-            />
-          </Field>
-          <Field orientation='horizontal'>
-            <Button
-              className='ml-auto w-15'
-              onClick={() =>
-                setSettings({
-                  ...settings,
-                  theme: {
-                    ...settings.theme,
-                    color: SettingsDefault.theme.color,
-                  },
-                })
-              }
-            >
-              Reset
-            </Button>
-          </Field>
-        </FieldGroup>
-      </FieldSet>
+      {settings.theme.mode === 'custom' && (
+        <>
+          <FieldSeparator />
+          <FieldSet>
+            <FieldLegend>Color scheme</FieldLegend>
+            <FieldGroup>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Background</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.background}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          background: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Foreground</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.foreground}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          foreground: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Card</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.card}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          card: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Primary</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.primary}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          primary: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Secondary</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.secondary}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          secondary: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Accent</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.accent}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          accent: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation={'horizontal'}>
+                <FieldLabel>Destructive</FieldLabel>
+                <Input
+                  type='color'
+                  value={settings.theme.color.destructive}
+                  onValueChange={(value) => {
+                    if (!value) return
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: {
+                          ...settings.theme.color,
+                          destructive: value,
+                        },
+                      },
+                    })
+                  }}
+                />
+              </Field>
+              <Field orientation='horizontal'>
+                <Button
+                  className='ml-auto w-15'
+                  onClick={() =>
+                    setSettings({
+                      ...settings,
+                      theme: {
+                        ...settings.theme,
+                        color: SettingsDefault.theme.color,
+                      },
+                    })
+                  }
+                >
+                  Reset
+                </Button>
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+        </>
+      )}
     </FieldGroup>
   )
 }
